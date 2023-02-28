c6611417b7bb4ed1cd4a3330b77d858c481b1f73
Volpe <renan.volpe@aluno.ifsp.edu.br>
2023-02-28 15:04:17 -0300

    hotfix import

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,5 +1,5 @@
 import 'package:finances_app/features/auth/pages/login_page.dart';
-import 'package:finances_app/features/home/home_page.dart';
+import 'package:finances_app/features/home/pages/home_page.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 
